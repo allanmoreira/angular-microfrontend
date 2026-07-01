21a5275202a02d914736c4b6ffaefb7cd837459c
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-06-30 22:34:55 -0300

    integrando os microfrontends

--- vendas/src/app/app.routes.ts
+++ vendas/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import {Home} from "./home/home";
+import {loadRemoteModule} from "@angular-architects/module-federation";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'produtos',
+        loadComponent: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4201/remoteEntry.js',
+            exposedModule: './Component'
+        }).then(mod => mod.App).catch(err => {
+            console.error('Erro ao carregar módulo remoto produtos:', err);
+            throw err;
+        }),
+    },
+    {
+        path: 'grafico',
+        loadComponent: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4202/remoteEntry.js',
+            exposedModule: './Component'
+        }).then(mod => mod.App).catch(err => {
+            console.error('Erro ao carregar módulo remoto grafico:', err);
+            throw err;
+        }),
+    },
+    { path: 'home', component: Home },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', redirectTo: '/home' },
+];
--- vendas/webpack.config.js
+++ vendas/webpack.config.js
@@ -6,8 +6,13 @@ const {
 module.exports = withModuleFederationPlugin({
   name: 'vendas',
 
+  remotes: {
+    "produtos": "http://localhost:4201/remoteEntry.js",
+    "grafico": "http://localhost:4202/remoteEntry.js",
+  },
+
   exposes: {
-    './Component': './src\app\app.ts',
+    './Component': './src/app/app.ts',
   },
 
   shared: {
