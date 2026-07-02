913d47770b109ced32ff01f62bf0a0c49581e892
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-07-01 23:35:50 -0300

    Tela gráfico

--- grafico/src/app/app.ts
+++ grafico/src/app/app.ts
@@ -1,4 +1,5 @@
-import {Component, signal} from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Chart } from 'chart.js/auto'
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,40 @@ import {Component, signal} from '@angular/core';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('grafico');
+export class App implements OnInit {
+
+  ngOnInit(): void {
+    const dados = [
+      { categoria: 'Eletronicos', valor: 10 },
+      { categoria: 'Roupas', valor: 15 },
+      { categoria: 'Decoração', valor: 5 },
+      { categoria: 'Ferramentas', valor: 20 },
+      { categoria: 'Acessórios', valor: 17 }
+    ];
+
+    const labels = dados.map(v => v.categoria);
+    const valores = dados.map(v => v.valor);
+
+    new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: 'Categorias Mais Vendidas',
+            data: valores,
+            backgroundColor: 'rgba(31, 82, 146, 0.6)'
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales:  {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
