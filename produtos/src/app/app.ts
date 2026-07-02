c7643f84a8f3f1a4b054624afe8d391bb4205ef7
Allan Moreira <allanfelipe.moreira@gmail.com>
2026-07-01 23:31:31 -0300

    Tela produtos

--- produtos/src/app/app.ts
+++ produtos/src/app/app.ts
@@ -1,11 +1,44 @@
-import {Component, signal} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+interface Produto {
+  nome: string;
+  valor: number;
+  imagem: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('produtos');
+  produtos: Produto[] = [
+    {
+      nome: 'Camiseta Angular',
+      valor: 79.90,
+      imagem: 'https://picsum.photos/id/1/200/150'
+    },
+    {
+      nome: 'Caneca Programador',
+      valor: 49.90,
+      imagem: 'https://picsum.photos/id/10/200/150'
+    },
+    {
+      nome: 'Mouse Gamer RGB',
+      valor: 129.90,
+      imagem: 'https://picsum.photos/id/25/200/150'
+    },
+    {
+      nome: 'Teclado Mecânico',
+      valor: 299.00,
+      imagem: 'https://picsum.photos/id/30/200/150'
+    },
+    {
+      nome: 'Headset Bluetooth',
+      valor: 199.90,
+      imagem: 'https://picsum.photos/id/40/200/150'
+    }
+  ];
 }
